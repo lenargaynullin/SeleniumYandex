1018ae03767db4080645592a1e7a3056701ac817
lenargaynullin <lenar.gaynullin88@gmail.com>
2022-02-05 15:45:34 +0300

    05.02.2022

--- TestSelenium/Yandex/Yandex.cs
+++ TestSelenium/Yandex/Yandex.cs
@@ -64,7 +64,8 @@ namespace TestSelenium
             // Клик Смартфоны
             IWebElement button3 = wait.Until(e => e.FindElement(By.XPath("//a[text()='Смартфоны']")));
             button3.Click();
-
+            Thread.Sleep(3000);
+            
             // Выбрать цену от 30000 рублей
             IWebElement price = wait.Until(e => e.FindElement(By.CssSelector("input[name='Цена от']")));
             price.SendKeys("30000");
@@ -74,19 +75,22 @@ namespace TestSelenium
             IWebElement screen = wait.Until(e => e.FindElement(By.XPath("//span[text()='6.5\" и больше']")));
             screen.Click();
             Thread.Sleep(3000);
-
-            
-            
+           
             // Apple
             IWebElement apple = wait.Until(e => e.FindElement(By.XPath("//span[text()='Apple']")));
             apple.Click();
             Thread.Sleep(3000);
-
+           
             // Nokia
             IWebElement nokia = wait.Until(e => e.FindElement(By.XPath("//span[text()='Nokia']")));
             nokia.Click();
             Thread.Sleep(3000);
 
+            // Realme
+            IWebElement realme = wait.Until(e => e.FindElement(By.XPath("//span[text()='realme']")));
+            realme.Click();
+            Thread.Sleep(3000);
+            
             // Samsung
             IWebElement samsung = wait.Until(e => e.FindElement(By.XPath("//span[text()='Samsung']")));
             samsung.Click();
@@ -97,24 +101,42 @@ namespace TestSelenium
             xiaomi.Click();
             Thread.Sleep(3000);
 
-            // ZTE
-            IWebElement zte = wait.Until(e => e.FindElement(By.XPath("//span[text()='ZTE']")));
-            zte.Click();
-            Thread.Sleep(3000);
-
             // Проверить что элементов на странице 10
             int ElementsCount = driver.FindElements(By.XPath("//article")).Count;
-                                  
-            Console.WriteLine("Количество элементов на странице" + ElementsCount);
-
             
-            // Assert.AreEqual(list.size(), 10, "Элементов на странице меньше 10");
-
+            var ElementsMore10 = false;
+            if (ElementsCount >= 10) 
+            {
+                ElementsMore10 = true; 
+                Console.WriteLine("Количество элементов на странице больше 10");
+            }
+            //Assert.AreEqual(true, ElementsMore10, "Элементов на странице меньше 10");
+            Thread.Sleep(3000);
             
             // Запомнить первый элемент в списке.
-            // Изменить Сортировку на другую(популярность или новизна).
+            IWebElement firstElement = wait.Until(e => e.FindElement(By.XPath("(//a[contains(@class,'_2f75n _24Q6d')])[1]")));
+            Thread.Sleep(3000);
+            
+            IWebElement firstElementTitle = wait.Until(e => e.FindElement(By.XPath("(//a[@title='Смартфон Samsung Galaxy M52 5G'])[1]")));
+            Thread.Sleep(3000); 
+                
+            // Изменить Сортировку на другую (по цене).
+            IWebElement sortingByPrice = wait.Until(e => e.FindElement(By.XPath("//button[text()='по цене']")));
+            sortingByPrice.Click();
+            Thread.Sleep(3000);
+            
             // Найти и нажать по имени запомненного объекта.
+            firstElementTitle.Click();   
+            Thread.Sleep(3000);
+            
             // Вывести цифровое значение его оценки.
+            IWebElement phoneRating = wait.Until(e => e.FindElement(By.XPath("(//span[@class='_2v4E8'])[2]")));
+            string rating = phoneRating.GetAttribute("innerText");
+            
+            Thread.Sleep(3000);
+            Console.WriteLine("Оценка равна = " + rating);
+            
+            
             // Закрыть браузер.
 
             Assert.AreEqual("https://yandex.ru/", driver.Url, "Яндекс Маркет не открылся");
