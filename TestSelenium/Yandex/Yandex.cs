9f9d3a03ad1871bb7d3adffb32f254c220c80315
lenargaynullin <lenar.gaynullin88@gmail.com>
2022-02-06 14:13:55 +0300

    06.02.2022

--- TestSelenium/Yandex/Yandex.cs
+++ TestSelenium/Yandex/Yandex.cs
@@ -15,7 +15,6 @@ namespace TestSelenium
         private string login, passwod;
         private bool isAuth = false;
 
-        const string YandexUrl = "https://yandex.ru/";
         const string YandexMarketUrl = "https://market.yandex.ru/";
 
         [OneTimeSetUp]
@@ -23,9 +22,9 @@ namespace TestSelenium
         {
             var chromeOptions = new ChromeOptions();
             
-            // Отключить "Браузером управляет автоматизированное ПО
-            //chromeOptions.AddAdditionalCapability("useAutomationExtension", false);
-            //chromeOptions.AddExcludedArgument("enable-automation");
+            // Отключить "Браузером управляет автоматизированное ПО"
+            chromeOptions.AddAdditionalCapability("useAutomationExtension", false);
+            chromeOptions.AddExcludedArgument("enable-automation");
             
             driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(), chromeOptions);
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
@@ -36,40 +35,31 @@ namespace TestSelenium
         {
             driver.Quit();
         }
-
-        //[Test]
-        //public void CheckYandexOpenTesting()
-        //{
-         //   driver.Navigate().GoToUrl(YandexUrl);
-         //   Assert.AreEqual("https://yandex.ru/", driver.Url, "Яндекс не открылся");
-            
-        //}
-
+        
         [Test]
         public void CheckYandexMarketTesting()
         {
-            
             driver.Manage().Window.Maximize(); // Окно во весь экран
             driver.Navigate().GoToUrl(YandexMarketUrl); // Открыть Yandex Market
-            
-            
-            
-            // Нажать Яндекс Маркет
-            //IWebElement market = wait.Until(e => e.FindElement(By.XPath("//div[text()='Маркет']")));
-            //market.Click();
             
             // Клик Электроника
             IWebElement button2 = wait.Until(e => e.FindElement(By.XPath("//span[text()='Электроника']")));
             button2.Click();
-
+            Thread.Sleep(2000);
+            
             // Клик Смартфоны
             IWebElement button3 = wait.Until(e => e.FindElement(By.XPath("//a[text()='Смартфоны']")));
             button3.Click();
             Thread.Sleep(2000);
             
             // Выбрать цену от 30000 рублей
-            IWebElement price = wait.Until(e => e.FindElement(By.CssSelector("input[name='Цена от']")));
-            price.SendKeys("30000");
+            IWebElement priceMin = wait.Until(e => e.FindElement(By.CssSelector("input[name='Цена от']")));
+            priceMin.SendKeys("30000");
+            Thread.Sleep(2000);
+            
+            // Выбрать цену до 39000 рублей
+            IWebElement priceMax = wait.Until(e => e.FindElement(By.CssSelector("input[name='Цена до']")));
+            priceMax.SendKeys("39000");
             Thread.Sleep(2000);
             
             // Диагональ экрана 6.5" и больше
@@ -102,7 +92,7 @@ namespace TestSelenium
             xiaomi.Click();
             Thread.Sleep(2000);
 
-            // Проверить что элементов на странице 10
+            // Проверить что элементов на странице больше 10
             int ElementsCount = driver.FindElements(By.XPath("//article")).Count;
             
             var ElementsMore10 = false;
@@ -111,45 +101,37 @@ namespace TestSelenium
                 ElementsMore10 = true; 
                 Console.WriteLine("Количество элементов на странице больше 10");
             }
-            //Assert.AreEqual(true, ElementsMore10, "Элементов на странице меньше 10");
             Thread.Sleep(2000);
             
-            // Запомнить первый элемент в списке.                                        
+            // Запомнить первый элемент в списке                                        
             IWebElement firstElement = wait.Until(e => e.FindElement(By.XPath("(//a[contains(@class,'_2f75n _24Q6d')]//span)[1]")));
-            string firstElementText = firstElement.GetAttribute("Text");
-            
-            
-            
-            
-            string firstElementXpath = "\"(//span[text()='" + firstElementText + "'])\"";
-            
-            Console.WriteLine("firstElementText = " + firstElementText);
-            Console.WriteLine("firstElementXpath = " + firstElementXpath);
+            string firstElementText = firstElement.Text;
             
             Thread.Sleep(2000);
-            // ---------------------------------------------------------------------------------------------------------
-                
-            // Изменить Сортировку на другую (по цене).
+            
+            // Изменить Сортировку на другую (по цене)
             IWebElement sortingByPrice = wait.Until(e => e.FindElement(By.XPath("//button[text()='по цене']")));
             sortingByPrice.Click();
-            Thread.Sleep(2000);
-            
-            // Найти по имени запомненного объекта
-            IWebElement firstElementFind = wait.Until(e => e.FindElement(By.XPath("//span[text()='Смартфон Samsung Galaxy M52 5G 6/128 ГБ, белый']")));
+            Thread.Sleep(5000);
+           
+            // Найти по имени запомненного объекта 
+            IWebElement firstElementFind = wait.Until(e => e.FindElement(By.XPath("//span[text()='" + firstElementText + "']")));
             firstElementFind.Click();
             Thread.Sleep(2000);
             
             // Вывести цифровое значение его оценки.
-            IWebElement phoneRating = wait.Until(e => e.FindElement(By.XPath("//span[@class='_2v4E8']")));
-            string rating = phoneRating.GetAttribute("text");
-            
+            IWebElement phoneRating = wait.Until(e => e.FindElement(By.XPath("(//span[@class='_2v4E8'])[2]")));
+            string rating = phoneRating.Text;
             Thread.Sleep(2000);
+            
+            
+            Console.WriteLine("Название телефона = " + firstElementText);
             Console.WriteLine("Оценка равна = " + rating);
             
             
             // Закрыть браузер.
-
-            Assert.AreEqual("https://yandex.ru/", driver.Url, "Яндекс Маркет не открылся");
+            
+            Assert.AreEqual(true, ElementsMore10, "Элементов на странице меньше 10");
 
         }
 
